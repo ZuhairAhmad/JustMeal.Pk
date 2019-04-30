54352335be71d57ec64c355ba33de1a3cedc15b0
ZuhairAhmad <37359621+ZuhairAhmad@users.noreply.github.com>
2019-04-30 10:49:01 +0500

    Add Files
    For testing

---
+++ 2019_01_25_115129_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('taxId')->unique();
+
+            $table->integer('item_id');
+            // foreign keys
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
